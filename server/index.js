e325ecd8753dd95b8e117510912a7516ebd3ca67
a.chernov <san4o95@mail.ru>
2016-11-13 18:37:18 +0300

    v0.1 STABLE

--- server/index.js
+++ server/index.js
@@ -1,5 +1,8 @@
-app.get("/folder/page", function(request, response)){
+var express = require("express");
+var app = express();
+
+app.get("/folder/page", function(request, response){
   var x = request.query.foo;
 
   response.send({foo:x});
-}
+});
