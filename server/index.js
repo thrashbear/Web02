26fb91bb783c7cba2b3c0ab464e5e025daba3b0c
a.chernov <san4o95@mail.ru>
2016-11-13 18:02:39 +0300

    Revert "kjsdfh"
    This reverts commit 398df7e2ae42d4d2c192a4490abaf4a267ab0de2.

--- server/index.js
+++ server/index.js
@@ -1,3 +1,8 @@
-app.get("/folder/page", function(request, response)){
-  var x = request
-}
+var express = require("express");
+var app = express();
+
+app.get("/", function(request, response){
+  response.send("<h1>XYU</h1>");
+});
+
+app.listen(591);
