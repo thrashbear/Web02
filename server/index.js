dff7c48f922ea9350c2bfe50afa68747b8520b1a
a.chernov <san4o95@mail.ru>
2016-11-20 18:24:27 +0400

    qwe

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
-var express = require("express");
+var express = require("express"),
+    cors = require("cors");
 var app = express();
+app.use(cors());
 
 app.get("/folder/page", function(request, response){
   var x = request.query.foo;
-
   response.send({foo:x});
 });
+
+app.listen(591);
